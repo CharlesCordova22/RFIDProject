c2d75aa8dc33ed13e1e853a4f0681298443e5e1f
Charles Cordova <131734392+CharlesCordova22@users.noreply.github.com>
2024-03-22 15:25:17 +0800

    Add files via upload

---
+++ RFIDProject/pages/homepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/homepage.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="main-container">
+        <div class="logo-name">
+            <div class="use-logo">
+                <h1>ATTENDIFY</h1>
+            </div>
+            <div class="use-text">
+                <p>USE ATTENDIFY TO SCAN YOUR ATTENDANCE</p>
+            </div>
+        </div>
+    </div>
+    <div class="scan-container">
+        <div class="scan-name">
+            <h3>SCAN HERE</h3>
+        </div>
+    </div>
+    <div class="arrow-container">
+        <div class="arrow-down"></div>
+    </div>
+    <div class="navbar">
+        
+    </div>
+</body>
+</html>
---
+++ RFIDProject/pages/locationLogin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/locationLogin.css">
+    <title>Location Logging</title>
+</head>
+<body>
+    <div class="location-container">
+        <div class="location">
+            <h1>LOCATION LOGGING</h1>
+        </div>
+    </div>
+    <div class="align-btn1">
+        <div class="comfort">
+            <form action="">
+                <button><h1>COMFORT ROOM</h1></button>
+            </form>
+        </div>
+        <div class="faculty">
+            <form action="">
+                <button><h1>FACULTY</h1></button>
+            </form>
+        </div>
+    </div>
+    <div class="align-btn2">
+        <div class="clinic">
+            <form action="">
+                <button><h1>CLINIC</h1></button>
+            </form>
+        </div>
+        <div class="office">
+            <form action="">
+                <button><h1>SCHOOL OFFICE</h1></button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
